a7e11b7660055204ab9f385961af5af2d9b9269e
Rylan <rylans@gmail.com>
2014-09-22 19:32:35 -0700

    Add Grunt for cssmin and csslint

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+	cssmin: {
+	  my_target: {
+		files: [{
+		  expand: true,
+		  cwd: 'stylesheets/',
+		  src: ['*.css', '!*.min.css'],
+		  dest: 'release/css/',
+		  ext: '.min.css'
+		}]
+	  }
+	},
+	csslint: {
+	  strict: {
+		options: {
+		  import: 2
+		},
+		src: ['stylesheets/**/*.css']
+	  }
+	}
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+  
+  grunt.registerTask('min', ['cssmin']);
+  grunt.registerTask('lint', ['csslint']);
+
+  grunt.registerTask('default', ['csslint', 'cssmin']);
+
+};
